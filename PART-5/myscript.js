f685380967984f313aa87775fbee8644ca76e35f
1kranthi <resanju192@gmail.com>
2024-08-24 15:41:27 +0530

    javascript start

---
+++ PART-5/myscript.js
@@ -0,0 +1,10 @@
+function calc(){
+
+    // const result = txtNum.value * txtNum.value;
+    // lblAnswer.innerHTML = result
+    let a=10;
+    let b=20;
+    let c=a+b;
+    lblAnswer.innerHTML=c;
+    alert(c)
+}
